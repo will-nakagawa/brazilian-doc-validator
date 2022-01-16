5ec3d71e1cdaab10516152722c32ec28302b4ada
William Nakagawa <saikoukanojo@gmail.com>
2022-01-16 20:07:50 -0300

    Cpf generator function refact, add eslint to the project (#6)
    * remove dist, refact cpf generator function
    
    * add eslint

--- src/validator/cpf.ts
+++ src/validator/cpf.ts
@@ -1,111 +1,128 @@
 /**
  * CPF validation
- * 
- * @param cpf string
- *  
- * @returns boolean
- * 
+ *
+ * @param {string} cpf
+ *
+ * @returns {boolean}
+ *
  */
 
 const cpfValidation = (cpf: string): Boolean => {
-    
-    if (!Boolean(cpf)) return false;
+  if (!Boolean(cpf)) return false;
 
-    const onlyNumbers: string = cpf.replace(/\D/g, "");
+  const onlyNumbers: string = cpf.replace(/\D/g, '');
 
-    if(onlyNumbers.length < 11) return false;
+  if (onlyNumbers.length < 11) return false;
 
-    let adition: number;
-    let remainder: number;
+  let adition: number;
+  let remainder: number;
 
-    adition = 0;
+  adition = 0;
 
-    if (onlyNumbers === "00000000000") return false;
-    
-    for (let i=1; i<=9; i++) {
-	    adition = adition + parseInt(onlyNumbers.substring(i-1, i)) * (11 - i);
-    }
-    
-    remainder = (adition * 10) % 11;
-    
-    if ((remainder == 10) || (remainder == 11)) remainder = 0;
-    if (remainder != parseInt(onlyNumbers.substring(9, 10)) ) return false;
-	
-    adition = 0;
-    
-    for (let i = 1; i <= 10; i++) {
-       adition = adition + parseInt(onlyNumbers.substring(i-1, i)) * (12 - i);
-    }
+  if (onlyNumbers === '00000000000') return false;
 
-    remainder = (adition * 10) % 11;
-    
-    if ((remainder == 10) || (remainder == 11)) remainder = 0;
-    if (remainder != parseInt(onlyNumbers.substring(10, 11) ) ) return false;
-    
-    return true;
+  for (let i = 1; i <= 9; i++) {
+    adition = adition + parseInt(onlyNumbers.substring(i - 1, i)) * (11 - i);
+  }
+
+  remainder = (adition * 10) % 11;
+
+  if ((remainder == 10) || (remainder == 11)) remainder = 0;
+  if (remainder != parseInt(onlyNumbers.substring(9, 10))) return false;
+
+  adition = 0;
+
+  for (let i = 1; i <= 10; i++) {
+    adition = adition + parseInt(onlyNumbers.substring(i - 1, i)) * (12 - i);
+  }
+
+  remainder = (adition * 10) % 11;
+
+  if ((remainder == 10) || (remainder == 11)) remainder = 0;
+  if (remainder != parseInt(onlyNumbers.substring(10, 11))) return false;
+
+  return true;
 };
 
 /**
  * CPF remove mask
- * 
- * @param cpf string
- *  
- * @returns string
- * 
+ *
+ * @param {string} cpf
+ *
+ * @returns {string}
+ *
  */
 
 const cpfAddMask = (cpf: string) => {
-    if(cpfValidation(cpf)) {
-        const onlyNumbers: string = cpf.replace(/\D/g, "");
-        return onlyNumbers.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})*/, '$1.$2.$3-$4');
-    }
-    return "";
+  if (cpfValidation(cpf)) {
+    const onlyNumbers: string = cpf.replace(/\D/g, '');
+    return onlyNumbers.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})*/, '$1.$2.$3-$4');
+  }
+  return '';
 };
 
 /**
  * CPF add mask
- * 
- * @param cpf string
- *  
- * @returns string
- * 
+ *
+ * @param {string} cpf
+ *
+ * @returns {string}
+ *
  */
 
-const cpfRemoveMask = (cpf: string):string => {
-    if(cpfValidation(cpf)) return cpf.replace(/\D/g, "");
-    return "";
+const cpfRemoveMask = (cpf: string): string => {
+  if (cpfValidation(cpf)) return cpf.replace(/\D/g, '');
+  return '';
 };
 
+/**
+ * CPF Generator
+ *
+ * @param {Record<string, any>} options
+ *
+ * @return {string}
+ *
+ */
 const cpfGenerate = (options: Record<string, any> = {mask: true}): string => {
+  const {mask} = options;
 
-    const {mask} = options;
+  const createArray =
+    (total: number, numero: number): number[] => Array.from(
+        Array(total),
+        () => numberRandom(numero),
+    );
 
-    let create_array = (total:number, numero:number): number[] => Array.from(Array(total), () => number_random(numero));
-    let number_random = (number:number) => (Math.round(Math.random() * number));
-    let mod = (dividendo:number, divisor:number):number => Math.round(dividendo - (Math.floor(dividendo / divisor) * divisor));
+  const numberRandom = (number: number) => (Math.round(Math.random() * number));
+  const mod = (dividendo: number, divisor: number): number =>
+    Math.round(dividendo - (Math.floor(dividendo / divisor) * divisor));
 
-    let array:number[] = create_array(9, 9);
+  const array: number[] = createArray(9, 9);
 
-    const formulaReducer = (number:number, array: number[]): number => {
-        return array.reduce( ( prevVal, elem, index ) => {
-            return prevVal + elem * (number - index);
-        }, 0)
-    }
+  const formulaReducer = (number: number, array: number[]): number => {
+    return array.reduce((prevVal, elem, index) => {
+      return prevVal + elem * (number - index);
+    }, 0);
+  };
 
-    let d1:number = formulaReducer(10, array);
+  let d1: number = formulaReducer(10, array);
 
-    d1 = 11 - (mod(d1, 11));
-    if (d1 >= 10) d1 = 0;
-        
-    let d2:number = (d1 * 2) + formulaReducer(11, array);
-    
-    d2 = 11 - (mod(d2, 11));
-    
-    if (d2 >= 10) d2 = 0;
-    
-    const newCpf: string = `${array.join('')}${d1}${d2}`;
+  d1 = 11 - (mod(d1, 11));
+  if (d1 >= 10) d1 = 0;
 
-    return (mask) ? cpfAddMask(newCpf) : newCpf ;
-}
+  let d2: number = (d1 * 2) + formulaReducer(11, array);
 
-export default { validate: cpfValidation, mask: cpfAddMask, unmask: cpfRemoveMask, generate: cpfGenerate };
+  d2 = 11 - (mod(d2, 11));
+
+  if (d2 >= 10) d2 = 0;
+
+  const newCpf: string = `${array.join('')}${d1}${d2}`;
+
+  return (mask) ? cpfAddMask(newCpf) : newCpf;
+};
+
+export default {
+  validate: cpfValidation,
+  mask: cpfAddMask,
+  unmask: cpfRemoveMask,
+  generate: cpfGenerate,
+};
--- tests/cnpj.test.ts
+++ tests/cnpj.test.ts
@@ -1,74 +1,72 @@
 import cnpj from '../src/validator/cnpj';
 
-const emptyCnpj = "";
-const validCnpjMasked = "31.257.435/0001-40";
-const validCnpjUnmasked = "31257435000140";
-const invalidCnpjMasked = "31.257.435/0001-4";
-const invalidCnpjUnmasked = "3125743500014";
+const emptyCnpj = '';
+const validCnpjMasked = '31.257.435/0001-40';
+const validCnpjUnmasked = '31257435000140';
+const invalidCnpjMasked = '31.257.435/0001-4';
+const invalidCnpjUnmasked = '3125743500014';
 
-describe("CNPJ Validation Test", () => {
-    it("Should validate a valid CNPJ number", () => {
-        const isValid = cnpj.validate(emptyCnpj);
-        expect(isValid).toBeFalsy();
-    });
+describe('CNPJ Validation Test', () => {
+  it('Should validate a valid CNPJ number', () => {
+    const isValid = cnpj.validate(emptyCnpj);
+    expect(isValid).toBeFalsy();
+  });
 
-    it("Should validate a valid CNPJ masked number", () => {
-        const isValid = cnpj.validate(validCnpjMasked);
-        expect(isValid).toBeTruthy();
-    });
+  it('Should validate a valid CNPJ masked number', () => {
+    const isValid = cnpj.validate(validCnpjMasked);
+    expect(isValid).toBeTruthy();
+  });
 
-    it("Should validate a valid CNPJ unmasked number", () => {
-        const isValid = cnpj.validate(validCnpjUnmasked);
-        expect(isValid).toBeTruthy();
-    });
+  it('Should validate a valid CNPJ unmasked number', () => {
+    const isValid = cnpj.validate(validCnpjUnmasked);
+    expect(isValid).toBeTruthy();
+  });
 
-    it("Should validate a invalid CNPJ masked number", () => {
-        const isValid = cnpj.validate(invalidCnpjMasked);
-        expect(isValid).toBeFalsy();
-    });
+  it('Should validate a invalid CNPJ masked number', () => {
+    const isValid = cnpj.validate(invalidCnpjMasked);
+    expect(isValid).toBeFalsy();
+  });
 
-    it("Should validate a invalid CNPJ unmasked number", () => {
-        const isValid = cnpj.validate(invalidCnpjUnmasked);
-        expect(isValid).toBeFalsy();
-    });
+  it('Should validate a invalid CNPJ unmasked number', () => {
+    const isValid = cnpj.validate(invalidCnpjUnmasked);
+    expect(isValid).toBeFalsy();
+  });
 });
 
-describe("CNPJ Mask Test", () => {
-    it("Should have a correct mask on a valid cnpj or return empty string", () => {
-        const newCnpj = cnpj.mask(validCnpjUnmasked);
-        expect(newCnpj).toBe(validCnpjMasked);
-    });
+describe('CNPJ Mask Test', () => {
+  it('Should have mask on a valid cnpj or return empty string', () => {
+    const newCnpj = cnpj.mask(validCnpjUnmasked);
+    expect(newCnpj).toBe(validCnpjMasked);
+  });
 
-    it("Should return empty string on invalid CNPJ", () => {
-        const newCnpj = cnpj.mask(invalidCnpjUnmasked);
-        expect(newCnpj).toBe("");
-    });
+  it('Should return empty string on invalid CNPJ', () => {
+    const newCnpj = cnpj.mask(invalidCnpjUnmasked);
+    expect(newCnpj).toBe('');
+  });
 });
 
-describe("CNPJ Unmask Test", () => {
-    it("Should have a correct unmask on a valid cnpj or return empty string", () => {
-        const newCnpj = cnpj.unmask(validCnpjMasked);
-        expect(newCnpj).toBe(validCnpjUnmasked);
-    });
+describe('CNPJ Unmask Test', () => {
+  it('Should be unmasked string on a valid cnpj or return empty string', () => {
+    const newCnpj = cnpj.unmask(validCnpjMasked);
+    expect(newCnpj).toBe(validCnpjUnmasked);
+  });
 
-    it("Should return empty string on invalid CNPJ", () => {
-        const newCnpj = cnpj.unmask(invalidCnpjUnmasked);
-        expect(newCnpj).toBe("");
-    });
+  it('Should return empty string on invalid CNPJ', () => {
+    const newCnpj = cnpj.unmask(invalidCnpjUnmasked);
+    expect(newCnpj).toBe('');
+  });
 });
 
-describe("CNPJ Generator Test", () => {
-    
-    it("Should generate a valid cnpj", () => {
-        const newCnpj = cnpj.generate();
-        const isValid = cnpj.validate(newCnpj);
-        expect(isValid).toBeTruthy();
-    });
+describe('CNPJ Generator Test', () => {
+  it('Should generate a valid cnpj', () => {
+    const newCnpj = cnpj.generate();
+    const isValid = cnpj.validate(newCnpj);
+    expect(isValid).toBeTruthy();
+  });
 
-    it("Should generate a valid cnpj and formmat", () => {
-        const newCnpj = cnpj.generate({format: true});
-        const isValid = cnpj.validate(newCnpj);
-        expect(isValid).toBeTruthy();
-    });
-
+  it('Should generate a valid cnpj and formmat', () => {
+    const newCnpj = cnpj.generate({format: true});
+    const isValid = cnpj.validate(newCnpj);
+    expect(isValid).toBeTruthy();
+  });
 });
